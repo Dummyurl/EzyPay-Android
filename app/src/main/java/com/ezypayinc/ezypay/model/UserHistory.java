89b8bc8512ded946da76b4285f5be9cc944dfabf
Gustavo Quesada Sanchez <quesada.tavo@gmail.com>
2017-11-16 23:55:41 -0600

    add object to handle user history

---
+++ app/src/main/java/com/ezypayinc/ezypay/model/UserHistory.java
@@ -0,0 +1,53 @@
+package com.ezypayinc.ezypay.model;
+
+public class UserHistory {
+
+    private String avatar;
+    private String name;
+    private String cost;
+    private String code;
+    private String paymentDate;
+
+    public UserHistory() {
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCost() {
+        return cost;
+    }
+
+    public void setCost(String cost) {
+        this.cost = cost;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getPaymentDate() {
+        return paymentDate;
+    }
+
+    public void setPaymentDate(String paymentDate) {
+        this.paymentDate = paymentDate;
+    }
+}
